27a127a8eff7d6f70432bd66d4631abbdf5f633d
Unknown <giovannikleincampigoto@gmail.com>
2018-05-02 21:25:19 -0300

    New changes to initial repo
    Created new changes to the initial repo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,22 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { 
+  Container,
+  Row, 
+  Col 
+} from 'reactstrap'
+
 class App extends Component {
-
-  componentDidMount() {
-    
-  }
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <Row>
+            <Col xs="12" lg="7">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+			</Col>
+          </Row>
+
+        </Container>
+        
       </div>
     );
   }
